af03e5180c121326e9308777435f8db1fd713cf4
ForTUp <xunanping@139.com>
2020-09-01 22:15:24 +0800

    token

--- src/utils/http.js
+++ src/utils/http.js
@@ -13,7 +13,7 @@ axios.interceptors.request.use(
     let token = sessionStorage.getItem('token')
     if (token) {  // 判断是否存在token，如果存在的话，则每个http header都加上token
       config.headers = {
-        'X-token': token
+        'token': token
       }
     }
     return config
