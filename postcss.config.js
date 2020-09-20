013a3485db210abf1f19757e22bdeb30757c41bf
ForTUp <xunanping@139.com>
2020-09-20 14:34:12 +0800

    自适应

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,14 @@
-module.exports = {
-  plugins: {
-    autoprefixer: {}
-  }
-}
+module.exports = () => ({
+    plugins: [
+        require('autoprefixer')(),
+        require('postcss-plugin-px2rem')({
+            rootValue: 16,
+            selectorBlackList: [/^html$/, /^body$/],
+            propList: ['*'],
+            replace: true,
+            minPixelValue: 1.01,
+            mediaQuery: false,
+            unitPrecision: 5
+        })
+    ]
+})
