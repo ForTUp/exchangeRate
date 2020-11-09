6557b11382efddf2f7d66bf96b3f196debdc723b
ForTUp <xunanping@139.com>
2020-11-09 22:54:20 +0800

    优化

--- vue.config.js
+++ vue.config.js
@@ -1,17 +1,29 @@
+const webpack = require('webpack')
+
 module.exports = {
-  devServer: {
-	open: true,  //设置自动打开
-	//port: 8080,  // 这个是默认端口
-    proxy: {
-      '/api': {
-		  //http://web.praymore.xyz/
-        target: 'http://hh.praymore.xyz',  //设置反向代理链接
-        changeOrigin: true,  //  是否跨域
-        pathRewrite: {
-			'^/api': '/api'  //  重定向
+	chainWebpack:config=>{
+		config.plugin('provide').use(webpack.ProvidePlugin,[
+			{
+				$:'jquery',
+				jquery:'jquery',
+				jQuery:'jquery',
+				'window.jQuery':'jquery'
+			}
+		])
+	},
+	devServer: {
+		open: true,  //设置自动打开
+		//port: 8080,  // 这个是默认端口
+		proxy: {
+		  '/api': {
+			  //http://web.praymore.xyz/
+			target: 'http://hh.praymore.xyz',  //设置反向代理链接
+			changeOrigin: true,  //  是否跨域
+			pathRewrite: {
+				'^/api': '/api'  //  重定向
+			}
+		  }
 		}
-      }
-    }
-  }
+	}
 }
 
